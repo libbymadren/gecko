b2514ea21387176bd048df5074804bdd4f742757
libbymadren <libbymadren@yahoo.com>
2023-05-29 21:36:35 -0400

    super basic m+ signups

--- cogs/mythicplus.py
+++ cogs/mythicplus.py
@@ -3,19 +3,31 @@ from discord.ext import commands
 from datetime import datetime as dt
 from utils import get_config
 
-class MyView(discord.ui.View): 
+# remove this if there's a better way to get the config data for the choices parameter
+config = get_config('./config/mythicplus.json')
+
+# TODO: for tank and healer, make sure that people cannot sign up for the role after someone already has
+# for DPS, make sure more than 3 people can't sign up
+class ButtonView(discord.ui.View): 
   @discord.ui.button(label="🛡 Tank", row=0, style=discord.ButtonStyle.primary)
   async def first_button_callback(self, button, interaction):
-    await interaction.response.send_message("You pressed me!")
+    embed.set_field_at(index=0,name="Tank:",value=f"<@{interaction.user.id}>")
+    await interaction.response.defer()
+    await interaction.edit_original_response(content=msg, embed=embed)
 
   @discord.ui.button(label="💚 Healer", row=0, style=discord.ButtonStyle.primary)
   async def second_button_callback(self, button, interaction):
-    await interaction.response.send_message("You pressed me!")
+    embed.set_field_at(index=1,name="Healer:",value=f"<@{interaction.user.id}>", inline=False)
+    await interaction.response.defer()
+    await interaction.edit_original_response(content=msg, embed=embed)
 
   @discord.ui.button(label="⚔️ DPS", row=0, style=discord.ButtonStyle.primary)
   async def third_button_callback(self, button, interaction):
-    await interaction.response.send_message("You pressed me!")
-
+    embed_dict = embed.to_dict()
+    dps_value = embed_dict["fields"][2]["value"] + f"\n<@{interaction.user.id}>"
+    embed.set_field_at(index=2,name="DPS:",value=dps_value)
+    await interaction.response.defer()
+    await interaction.edit_original_response(content=msg, embed=embed)
 
 class MythicPlus(commands.Cog):
   def __init__(self, bot, config):
@@ -28,25 +40,24 @@ class MythicPlus(commands.Cog):
     if channel is not None:
       await channel.send(f'Welcome {member.mention}.')
 
-
-  # TODO - Can dungeons be stored as a list to provide options to the user, instead of freeform?
   @commands.slash_command(description = "Starts a key group.")
-  async def keys(self, ctx, level: discord.Option(int), dungeon: discord.Option(str)):
-    try: 
-      embed = discord.Embed(
-        title=f"+{level} {self.config['season2']['dungeons'][dungeon]}",
-        description="",
-        colour=0x00b0f4,
-        timestamp=dt.now()
-      )
-      
-      embed.add_field(name="Tank", value="This is the field value.")
-      embed.add_field(name="Healer", value="This is the field value.", inline=False)
-      embed.add_field(name="DPS", value="This is the field value.")
-          
-      await ctx.respond(f"<@{ctx.author.id}> is looking for a mythic+ group!", embed=embed, view=MyView())
-    except KeyError:
-      await ctx.respond("Invalid dungeon code. Please use /keycodes to see a list of all possible dungeon codes.")
+  async def keys(self, ctx, level: discord.Option(int), dungeon: discord.Option(str, choices = config['season2']['dungeons'])):
+    # again, this is bad practice, but I'm not familiar enough w/ this to know how I should maintain this embed's state
+    global embed
+    embed = discord.Embed(
+      title=f"+{level} {self.config['season2']['dungeons'][dungeon.upper()]}",
+      description="",
+      colour=0x00b0f4,
+      timestamp=dt.now()
+    )
+    
+    embed.add_field(name="Tank:", value="This is the field value.")
+    embed.add_field(name="Healer:", value="This is the field value.", inline=False)
+    embed.add_field(name="DPS:", value="This is the field value.")
+        
+    global msg
+    msg = f"<@{ctx.author.id}> is looking for a mythic+ group!"
+    await ctx.respond(msg, embed=embed, view=ButtonView())
 
 def setup(bot):
   bot.add_cog(MythicPlus(bot, get_config('./config/mythicplus.json')))
